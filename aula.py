564890f46952a5a124eeedaf624108f21059ce65
Stephanne Leão <stephanneleao@gmail.com>
2026-03-03 17:10:28 -0300

    Atividade da aula 5 "Variáveis"

--- aula.py
+++ aula.py
@@ -7,7 +7,7 @@ nome = input("Qual é o seu nome? ") # Este é um comentário inline
 print("Seja bem-vinda ao curso de Python,", nome)
 
 # Vou quebrar a execução:
-  print("Este é um erro!") # Erro de identação
+#  print("Este é um erro!") # Erro de identação
 
 '''
 Este é um comentário
@@ -15,3 +15,23 @@ de múltiplas linhas :)
 '''
 idade = input("Qual é a sua idade? ")
 print("Você tem", idade, "anos")
+
+# Variáveis
+
+# Reatribução de valores
+nome = "Fulana"
+idade = 65
+
+'''
+Nomes de variáveis "proibidos":
+1iniciandocomnumero
+kebab-case
+class
+'''
+
+print("Nome:", nome, "Idade:", idade)
+
+# Atribuição múltipla
+
+a = b = c = 10
+print(a,b,c)

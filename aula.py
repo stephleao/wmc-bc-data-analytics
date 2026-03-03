f15f2c8f4ff8233dfb6dcc92518638fa2919f920
Stephanne Leão <stephanneleao@gmail.com>
2026-03-03 16:38:47 -0300

    Atividade da aula 3 "Entrada e saída de dados"

--- aula.py
+++ aula.py
@@ -1,1 +1,7 @@
-print("Olá, seja bem-vinda ao curso de Python!")
+print("Olá!")
+
+nome = input("Qual é o seu nome? ")
+print("Seja bem-vinda ao curso de Python,", nome)
+
+idade = input("Qual é a sua idade? ")
+print("Você tem", idade, "anos")

16cef01ba655d291ba66cd990bcedf2566fd2c6b
Stephanne Leão <stephanneleao@gmail.com>
2026-03-04 14:11:42 -0300

    Atividade 1 da aula 9 "Operações Matemáticas"

--- aula.py
+++ aula.py
@@ -1,27 +1,23 @@
-# Trabalhando com textos - String
+# Operações Matemáticas
 
-nome = "Stephanne"
+"""
+Exemplos de operadores:
++ soma
+- subtração
+* multiplicação
+/ divisão - vai retornar decimal
+/ divisão inteira - vai retornar inteiro
+% resto da divisão
+** potenciação
+"""
 
-print(nome)
+valorA = 15
+valorB = 4
 
-# Slicing
-# Pega apenas o caractere da posição 1, que é o ´t´
-print(nome[1])
-# Pega caracteres a partir da posição 0 até antes da posição 5, ou seja 4
-print(nome[0:5])
-
-# Quebras de linha
-frase1 = "Quanto mais estudo, \nmais sinto que minha mente \nnisso é insaciável."
-print(frase1)
-
-# Quebra como texto pre-formatado - 3x aspas simples ou duplas
-# Escape de caracteres - usa `\` antes
-frase2 = """\"Quanto mais estudo,
-mais sinto que minha mente
-nisso é insaciável.\""""
-print(frase2)
-
-# Ao usar aspas simples, não é necessário fazer escape das aspas duplas
-# Poderia usar as aspas simples triplicadas para deixar pre-formatado
-frase3 = '"Quanto mais estudo, mais sinto que minha mente nisso é insaciável."'
-print(frase3)
+print("soma:", valorA + valorB)
+print("subtração:", valorA - valorB)
+print("multiplicação:", valorA * valorB)
+print("divisão:", valorA / valorB)
+print("divisão inteira:", valorA // valorB)
+print("resto da divisão:", valorA % valorB)
+print("potenciação:", valorA ** valorB)

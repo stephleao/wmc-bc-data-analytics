c568ceae95ed35dbbf5d71b62034d098dd947d84
Stephanne Leão <stephanneleao@gmail.com>
2026-03-04 13:54:05 -0300

    Atividade 3 da aula 8 "Trabalhando com textos"

--- aula.py
+++ aula.py
@@ -15,7 +15,13 @@ frase1 = "Quanto mais estudo, \nmais sinto que minha mente \nnisso é insaciáve
 print(frase1)
 
 # Quebra como texto pre-formatado - 3x aspas simples ou duplas
-frase2 = """Quanto mais estudo,
+# Escape de caracteres - usa `\` antes
+frase2 = """\"Quanto mais estudo,
 mais sinto que minha mente
-nisso é insaciável."""
+nisso é insaciável.\""""
 print(frase2)
+
+# Ao usar aspas simples, não é necessário fazer escape das aspas duplas
+# Poderia usar as aspas simples triplicadas para deixar pre-formatado
+frase3 = '"Quanto mais estudo, mais sinto que minha mente nisso é insaciável."'
+print(frase3)

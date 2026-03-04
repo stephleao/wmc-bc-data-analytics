d57f114b00cdd3d5d6f2ad20f23f8967340af098
Stephanne Leão <stephanneleao@gmail.com>
2026-03-04 13:52:50 -0300

    Atividade 2 da aula 8 "Trabalhando com textos"

--- aula.py
+++ aula.py
@@ -9,3 +9,13 @@ print(nome)
 print(nome[1])
 # Pega caracteres a partir da posição 0 até antes da posição 5, ou seja 4
 print(nome[0:5])
+
+# Quebras de linha
+frase1 = "Quanto mais estudo, \nmais sinto que minha mente \nnisso é insaciável."
+print(frase1)
+
+# Quebra como texto pre-formatado - 3x aspas simples ou duplas
+frase2 = """Quanto mais estudo,
+mais sinto que minha mente
+nisso é insaciável."""
+print(frase2)

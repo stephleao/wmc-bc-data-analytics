b26be7b8404473ce2ac735cbc29613f060dca2ba
Stephanne Leão <stephanneleao@gmail.com>
2026-03-04 15:17:38 -0300

    Atividade da aula 12 "Fazendo o código tomar decisões"

--- aula.py
+++ aula.py
@@ -1,36 +1,15 @@
-# Operadores relacionais e precedência
-# Retornam booleano - true/false
+# Fazendo o código tomar decisões
+# Condições com if, else, elif
 
-"""
-Exemplos de operadores:
-> maior que
-< menor que
->= maior ou igual
-<= menor ou igual
-== igual a
-!= diferente de
-"""
+idade = 65
+eh_membro = True
 
-valorA = int(input("Qual o valor A? "))
-valorB = int(input("Qual o valor B? "))
-
-print("A maior que B:", valorA > valorB)
-print("A menor que B:", valorA < valorB)
-print("A maior ou igual B:", valorA >= valorB)
-print("A menor ou igual B:", valorA <= valorB)
-print("A igual a B:", valorA == valorB)
-print("A diferente de B:", valorA != valorB)
-
-# Calcula e compara
-resultado = valorA + valorB > 2
-print(resultado)
-
-# Precedência
-
-# Priorizará a multiplicação
-resultado = valorA + valorB * 2
-print(resultado)
-
-# Priorizará a soma
-resultado = (valorA + valorB) * 2
-print(resultado)
+if idade >= 60:
+  if(eh_membro):
+    print("Desconto 30%")
+  else:
+    print("Desconto 20%")
+elif idade >= 50:
+  print("Cashback 10%")
+else:
+  print("Sem desconto")

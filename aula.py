56da087830714dcf2f019ebbdac59d9db59247ad
Stephanne Leão <stephanneleao@gmail.com>
2026-03-05 14:07:17 -0300

    Atividade 2 da aula 12 "Fazendo o código tomar decisões"

--- aula.py
+++ aula.py
@@ -1,15 +1,16 @@
 # Fazendo o código tomar decisões
 # Condições com if, else, elif
 
-idade = 65
-eh_membro = True
+nota1 = 7
+nota2 = 8
+nota3 = 5
 
-if idade >= 60:
-  if(eh_membro):
-    print("Desconto 30%")
-  else:
-    print("Desconto 20%")
-elif idade >= 50:
-  print("Cashback 10%")
+# a divisão retorna float, arrendondamos para 2 casas
+media = round((nota1 + nota2 + nota3) / 3, 2)
+
+if media >= 7:
+  print("Aprovada - Média: ", media)
+elif media >= 5:
+  print("Recuperação - Média: ", media)
 else:
-  print("Sem desconto")
+  print("Reprovada - Média: ", media)

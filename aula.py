d59d132074658ab246f777a8d2d3e91610a9afb5
Stephanne Leão <stephanneleao@gmail.com>
2026-03-05 17:13:26 -0300

    Atividade 4 da aula 13 "Laços de Repetição com FOR"

--- aula.py
+++ aula.py
@@ -4,5 +4,9 @@
 tecnologias = ["Python", "Dados", "IA"]
 
 # Percorre cada item da lista imprimindo a informação armazenada
-for item in tecnologias:
-  print(item)
+# range(len()) faz a mesma coisa do outro exemplo, mas o foco é na posição,
+# então será útil para manipula-lo - modificar seu valor, comparar valores
+# len() retorna o tamanho da lista e o range() cria a sequência numérica
+for item in range(len(tecnologias)):
+  # `item` aqui retorna a posição, não o valor
+  print(tecnologias[item])

fe74b2e14d312a99e94402d2735a36faa3887cab
Stephanne Leão <stephanneleao@gmail.com>
2026-03-07 19:41:07 -0300

    Atividade 3 da aula 18 "Trabalhando com funções"

--- aula.py
+++ aula.py
@@ -6,8 +6,16 @@
 # def vem de 'define function' ou só 'defina'
 # Passando um parâmetro, que é como uma variável que guardará um valor ao usar
 # a função
-def saudacao(nome):
-  print(f"Seja bem-vinda, {nome}")
+def saudacao(nome, estado): # permite vários parâmetros
+  print(f"Seja bem-vinda, {nome}. Muito bom ver alguém de {estado}.")
 
 # Dará erro se não passar o valor do parâmetro que a função espera
-saudacao("Steph")
+saudacao("Steph", "RJ")
+
+# Exemplo de função para cálculo
+def soma(a, b):
+  # Devolvendo um valor (que poderá ser usado em outras partes do código)
+  return a + b
+
+resultado = soma(5, 3)
+print(resultado)

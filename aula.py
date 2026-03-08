bf32ac0060857351568643c43bd3af8b7ec1bb20
Stephanne Leão <stephanneleao@gmail.com>
2026-03-07 21:04:36 -0300

    Atividade 1 da aula 20 "Acessando e Lendo Arquivos"

--- aula.py
+++ aula.py
@@ -1,21 +1,21 @@
-# Criando arquivos
+# Acessando e Lendo Arquivos
 
 arquivo = "alunas.txt"
 
-# Cria/abre um arquivo para escrita em UTF-8, e garante que ele seja fechado
+# Acessa o arquivo para leitura em UTF-8, e garante que ele seja fechado
 # corretamente
-with open(arquivo, mode="w", encoding="utf-8") as lista:
+with open(arquivo, mode="r", encoding="utf-8") as lista:
   # `with ... as ...` : gerenciador de contexto -> dá mais segurança e controle
   # `with ...` : garante que o arquivo será automaticamente fechado ao final do
   # bloco identado, mesmo que ocorra erro no bloco -> boa prática
   # `open()` : abre um objeto de arquivo
-  # `mode="w"` : "write", escrita. Se existir, será sobrescrito; senão, criado
+  # `mode="r"` : "read", leitura
   # `encoding="utf-8"` : trata corremente caracteres especiais
   # `as` : associa a uma variável
   # `lista` : variável que representa o objeto do arquivo e poderá manipulá-lo
   # com os métodos
 
-  # write() é o método de escrita
-  lista.write("Ana\n")
-  lista.write("Beatriz\n")
-  lista.write("Gisele\n")
+  # read() é o método de leitura
+  conteudo = lista.read()
+
+print(conteudo)

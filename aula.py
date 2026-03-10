16380855061554c959b1e64fdfb96a8256820065
Stephanne Leão <stephanneleao@gmail.com>
2026-03-10 13:43:58 -0300

    Atividade 1 da aula 23 "Tratamento de Erros"

--- aula.py
+++ aula.py
@@ -1,15 +1,10 @@
-# Deletando Arquivos
+# Tratamento de Erros
+# Evita que o programa pare, trabalhando as excessões
 
-# Importando módulo OS - que interage com o sistema operacional, para acessar
-# os arquivos
-import os
-
-arquivo = "alunas.txt"
-
-# Checa se o arquivo existe no sistema
-# "Se o caminho deste arquivo existir no SO"
-if os.path.exists(arquivo):
-  os.remove(arquivo) # Remove o arquivo
-  print("Arquivo deletado")
-else:
-  print("Arquivo não encontrado")
+# try/except é como if/else: tente fazer, se der erro, faça tal coisa
+try:
+  # se digitar qualquer coisa além de número, dará erro
+  numero = int(input("Digite um número: "))
+  print(10 / numero)
+except: # Evita que o programa pare devido ao erro
+  print("Ocorreu um erro")

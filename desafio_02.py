656d0e9278e339ff1f4175ed65592a39135366c6
Stephanne Leão <stephanneleao@gmail.com>
2026-03-10 22:51:22 -0300

    Melhoria do Desafio 2: Uma função só

--- desafio_02.py
+++ desafio_02.py
@@ -8,50 +8,27 @@ Crie uma função que receba uma lista de números e retorne:
 - a média dos valores
 """
 
-# Função que obtém o maior valor
-def obter_max(lista):
-  # Variavel começa com o primeiro item da lista para comparar
-  comparador = lista[0]
-
-  # Percorre a lista de numeros
-  for numero in lista:
-    # Checa entre o comparador e o numero para poder guardar o valor
-    if numero > comparador:
-      comparador = numero
-
-  return comparador
-
-# Função que obtém o menor valor
-# Mesma lógica da função max, porém verificando se é menor
-def obter_min(lista):
-  comparador = lista[0]
-
-  for numero in lista:
-    if numero < comparador:
-      comparador = numero
-
-  return comparador
-
-# Função que calcula a média
-def obter_media(lista):
-  media = 0
-
-  # Percorre a lista de numeros
-  for numero in lista:
-    media += numero # Soma os valores e guarda
-
-  # divide a media pela quantidade da lista e retorna
-  return media / len(lista)
-
-# Função que concatena os resultados das demais
 def obter_valores(lista):
-  maximo = obter_max(lista)
-  minimo = obter_min(lista)
-  media = obter_media(lista)
+  soma = 0
+  # Variaveis de max/min começam com o primeiro item da lista para comparar
+  maximo = lista[0]
+  minimo = lista[0]
+
+  # Percorre a lista de numeros
+  for numero in lista:
+    soma += numero # Soma os valores e guarda
+
+    # Verifica se o numero é maior/menor
+    if numero > maximo:
+      maximo = numero
+    elif numero < minimo:
+      minimo = numero
+
+  media = soma / len(lista) # divide soma pela quantidade da lista
 
   return maximo, minimo, media
 
-# Testando as funções
+# Testando a função
 
 lista_numeros = [345, 185, 46, 234]
 
